780bca02166a96c68d8a89fb15d2b2ef861a28f1
Katrina <32313004+brownkat6@users.noreply.github.com>
2020-02-10 11:14:18 -0500

    added DriveTrain import in TurnDegrees command

--- src/main/java/frc/robot/commands/drive/TurnDegrees.java
+++ src/main/java/frc/robot/commands/drive/TurnDegrees.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotConfig;
 import frc.robot.sensors.IMU;
+import frc.robot.subsystems.DriveTrain;
 
 public class TurnDegrees extends CommandBase {
     private double setpoint;
