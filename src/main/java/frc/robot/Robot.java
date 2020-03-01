33ead8c458d1fdfd73437352b2c0b545a1be8ea5
Katrina <32313004+brownkat6@users.noreply.github.com>
2020-03-01 10:09:40 -0500

    idk

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -124,6 +124,7 @@ public class Robot extends TimedRobot {
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
         // this line or comment it out.
+        IMU.getInstance().
         if (autonomousCommand != null) {
             autonomousCommand.cancel();
         }
