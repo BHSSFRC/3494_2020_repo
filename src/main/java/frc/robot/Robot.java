7a82412c022be4493a129e3045773713586c71f4
Katrina <32313004+brownkat6@users.noreply.github.com>
2020-03-02 08:57:11 -0500

    end of gainesville

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -124,6 +124,7 @@ public class Robot extends TimedRobot {
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
         // this line or comment it out.
+        CommandScheduler.getInstance().cancelAll();
         if (autonomousCommand != null) {
             autonomousCommand.cancel();
         }
